6739df6bb5cf5cec23fe0393ec8cf2047fe7c1b6
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-10-19 12:36:13 -0400

    Updated ARPlacement script

--- Assets/Resoources/Scripts/ARPlacement.cs
+++ Assets/Resoources/Scripts/ARPlacement.cs
@@ -24,7 +24,14 @@ public class ARPlacement : MonoBehaviour
     
     void Update()
     {
+
+        if (spawndObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+
         UpdatePlacementIndicator();
+        UpdatePlacementPose();
     }
 
 
@@ -39,6 +46,26 @@ public class ARPlacement : MonoBehaviour
         {
             placementIndicator.SetActive(false);
         }
+    }
+
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRayCastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+
+    }
+
+    void ARPlaceObject()
+    {
+        spawndObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
     }
 
 }
