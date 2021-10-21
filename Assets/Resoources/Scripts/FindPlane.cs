d3cc7f1fe746b67b9501e756f8a2cfccb774aca5
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-10-20 22:56:15 -0400

    Updated FindPlane.cs

--- Assets/Resoources/Scripts/FindPlane.cs
+++ Assets/Resoources/Scripts/FindPlane.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FindPlane : MonoBehaviour
 {
+    public System.Action<PlaneData> OnValidPlaneFound { get; internal set; }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Resoources/Scripts/MoveObjectToPlane.cs
+++ Assets/Resoources/Scripts/MoveObjectToPlane.cs
@@ -7,5 +7,20 @@ public class MoveObjectToPlane : MonoBehaviour
 
     private FindPlane PlaneFinder;
     void Awake() { PlaneFinder = FindObjectOfType<FindPlane>(); }
-   
+    void Start()
+    {
+        DisableObject();
+        //PlaneFinder.OnValidPlaneFound += UpdateTransform; PlaneFinder.OnValidPlaneNotFound += DisableObject;
+    }
+
+   /* private void UpdateTransform(PlaneData Plane)   
+    {        gameObject.SetActive(true);       
+        transform.SetPositionAndRotation(Plane.Position,Plane.Rotation);  
+    }*/
+    private void DisableObject()  
+    {
+        gameObject.SetActive(false);   
+    } 
+
+
 }
